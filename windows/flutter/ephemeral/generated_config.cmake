e47f01f92dff9fb9127654ae6c251674417c30a6
Guilherme Henrique Gonçalves <147348431+Guirens@users.noreply.github.com>
2024-10-03 14:11:29 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\arquivos de progamas\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\biblioteca\\Downloads\\jogos" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\arquivos de progamas\\flutter"
+  "PROJECT_DIR=E:\\biblioteca\\Downloads\\jogos"
+  "FLUTTER_ROOT=E:\\arquivos de progamas\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\biblioteca\\Downloads\\jogos\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\biblioteca\\Downloads\\jogos"
+  "FLUTTER_TARGET=E:\\biblioteca\\Downloads\\jogos\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC81NWVhZTY4NjRiMjk2ZGQ5ZjQzYjJjYzc1NzdlYzI1NmU1YzMyYThkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\biblioteca\\Downloads\\jogos\\.dart_tool\\package_config.json"
+)
